924538b3fec7f81453b9f5d2ce725cb50222e9b4
AndresGB <andresgamboabaldi@gmail.com>
2020-06-01 20:18:42 -0400

    Adding practice

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <span>Inventario Bibliotecario</span>
+    <br />
+    <select v-model="selectedCategory">
+      <option>Books</option>
+      <option>Magazines</option>
+      <option>Newspapers</option>
+      <option>Others</option>
+      <option>All</option>
+    </select>
+    <br />
+    <input v-model="selectedName" placeholder="Escriba el nombre" />
+    <br />
+    <div :key="item.code" v-for="item in filteredList">
+      <span>{{ item.code }}</span>
+      <span>{{ item.name }}</span>
+      <span>{{ item.description }}</span>
+      <span>{{ item.category }}</span>
+      <br />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import * as data from "../data.json";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      selectedName: "",
+      selectedCategory: "All",
+      filteredlist: "",
+      list: data.obj
+    };
+  },
+  computed: {
+    filteredList() {
+      const categoryList =
+        this.selectedCategory === "All" || this.selectedCategory === ""
+          ? this.list
+          : this.list.filter(item => item.category === this.selectedCategory);
+      return this.selectedName === ""
+        ? categoryList
+        : categoryList.filter(item =>
+            item.name.toLowerCase().includes(this.selectedName.toLowerCase())
+          );
+    }
   }
 };
 </script>
